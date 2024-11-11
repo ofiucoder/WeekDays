9b995a38c083da8c83a3abf25247c0623eafbcf8
Alberto Casas <Alberto.Casas.ofiuco@gmail.com>
2024-11-11 10:00:45 +0100

    feat: recuperado

---
+++ src/test/java/dev/ofiucoder/weekdays/HandlerWeekDaysTest.java
@@ -0,0 +1,69 @@
+package dev.ofiucoder.weekdays;
+
+import org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+
+import static   
+ org.junit.jupiter.api.Assertions.*;   
+
+public class HandlerWeekDaysTest {
+    
+    private final HandlerWeekDays handlerWeekDays = new HandlerWeekDays();
+
+    @Test
+    public void testCreateList() {
+        List<String> days = handlerWeekDays.getDays();
+        assertEquals(7, days.size());
+        assertTrue(days.contains("Lunes"));
+        assertTrue(days.contains("Viernes"));
+        assertTrue(days.contains("Domingo"));
+    }
+
+    @Test
+    public void testGetNumberOfDays() {
+        assertEquals(7, handlerWeekDays.getNumberOfDays());
+    }
+
+    @Test
+    public void testRemoveDay() {
+        handlerWeekDays.removeDay("Lunes");
+        assertEquals(6, handlerWeekDays.getNumberOfDays());
+        assertFalse(handlerWeekDays.containsDay("Lunes"));
+    }
+
+    @Test
+    public void testGetDay() {
+        assertEquals("Miércoles", handlerWeekDays.getDay(2));
+        assertThrows(IndexOutOfBoundsException.class, () -> handlerWeekDays.getDay(10));
+    }
+
+    @Test
+    public void testContainsDay() {
+        assertTrue(handlerWeekDays.containsDay("Viernes"));
+        assertFalse(handlerWeekDays.containsDay("Foobar"));
+    }
+
+    @Test
+    public void testSortDaysAlphabetically() {
+        handlerWeekDays.sortDaysAlphabetically();
+        assertEquals("Domingo", handlerWeekDays.getDay(6));
+        assertEquals("Lunes", handlerWeekDays.getDay(0));
+    }
+
+    @Test
+    public void testClearDays() {
+        handlerWeekDays.clearDays();
+        assertEquals(0, handlerWeekDays.getNumberOfDays());
+    }
+
+
+
+
+}
